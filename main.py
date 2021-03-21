bdaae21d5feff0b2fa3a5c4d8d7a6e6d3b742210
Ruletka <Ruletka.com@yandex.ru>
2021-03-21 10:45:00 +0300

    data

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 # Интерфейс
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QComboBox, QLabel
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QComboBox, QLabel, QComboBox
 from PyQt5.QtGui import QFont, QPixmap
 from PyQt5 import QtCore
 import matplotlib.pyplot as plt
 from PyQt5 import uic
+import json
+import numpy as np
+
+month_per_day = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 
 class MainWindow(QMainWindow):
@@ -12,10 +16,20 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.secondWin = None
         uic.loadUi('main.ui', self)
-        a = plt.plot(list(range(1, 100)))
+
+        with open("cities/Западный.json") as file:
+            data = json.loads(file.read())
+        plt.plot([np.median(j) for j in [data[i * 365: (i + 1) * 365] for i in range(len(data) // 365)]])
         plt.ylabel('Температура')
         plt.savefig('plot.png')
         self.label.setPixmap(QPixmap('plot.png').scaledToWidth(self.label.width()).scaledToHeight(self.label.height()))
+
+        # Организация времени
+        self.day.clear()
+        self.day.addItems(map(str, range(1, month_per_day[int(self.month.currentText()) - 1] + 1)))
+        func_changed = lambda *args, **kwargs: self.day.clear() or\
+        self.day.addItems(map(str, range(1, month_per_day[int(self.month.currentText()) - 1] + 1)))
+        self.month.currentIndexChanged.connect(func_changed)
 
 
 if __name__ == '__main__':
